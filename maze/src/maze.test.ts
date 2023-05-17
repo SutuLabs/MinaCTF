9ea38e5b3d0d6057e38f9d7adf0d9dd15e127df6
Liang Shuang <liangshuang@sutu.tech>
2023-05-17 03:20:58 +0000

    update test code

--- maze/src/maze.test.ts
+++ maze/src/maze.test.ts
@@ -1,12 +1,10 @@
 import { Maze, MazeContract } from './maze';
 import {
   Field,
-  Bool,
   PrivateKey,
   PublicKey,
   Mina,
   AccountUpdate,
-  Signature,
 } from 'snarkyjs';
 
 import { serializeToMaze } from "./mazeGenerator";
@@ -25,34 +23,32 @@ const defMaze = serializeToMaze(`
 11111111111111111111111
 `.trim()
 );
-// console.log(defMaze.maze)
-// new Maze(Field(defMaze.maze), Field(defMaze.start), Field(defMaze.end)).printState();
 
 describe('maze', () => {
-  let player1: PublicKey,
-    player1Key: PrivateKey,
-    player2: PublicKey,
+  let playerPublicKey: PublicKey,
+    playerPrivateKey: PrivateKey,
     zkAppAddress: PublicKey,
     zkAppPrivateKey: PrivateKey;
 
   beforeEach(async () => {
     let Local = Mina.LocalBlockchain({ proofsEnabled: false });
     Mina.setActiveInstance(Local);
-    [{ publicKey: player1, privateKey: player1Key }, { publicKey: player2 }] =
-      Local.testAccounts;
+    [{ publicKey: playerPublicKey, privateKey: playerPrivateKey }] = Local.testAccounts;
     zkAppPrivateKey = PrivateKey.random();
     zkAppAddress = zkAppPrivateKey.toPublicKey();
   });
 
-  it('generates and deploys contract', async () => {
+  it('deploys & accepts a correct move', async () => {
     const zkApp = new MazeContract(zkAppAddress);
-    const txn = await Mina.transaction(player1, () => {
-      AccountUpdate.fundNewAccount(player1);
+
+    // deploy
+    let txn = await Mina.transaction(playerPublicKey, () => {
+      AccountUpdate.fundNewAccount(playerPublicKey);
       zkApp.deploy();
       zkApp.startGame(Field(defMaze.maze), Field(defMaze.start), Field(defMaze.end));
     });
     await txn.prove();
-    await txn.sign([zkAppPrivateKey, player1Key]).send();
+    await txn.sign([zkAppPrivateKey, playerPrivateKey]).send();
 
     // check maze status
     const m = zkApp.maze.get();
@@ -60,29 +56,16 @@ describe('maze', () => {
     const p = zkApp.position.get();
     const e = zkApp.end.get();
     new Maze(m, p, e).printState();
-  });
-
-  it('deploys & accepts a correct move', async () => {
-    const zkApp = new MazeContract(zkAppAddress);
-
-    // deploy
-    let txn = await Mina.transaction(player1, () => {
-      AccountUpdate.fundNewAccount(player1);
-      zkApp.deploy();
-      zkApp.startGame(Field(defMaze.maze), Field(defMaze.start), Field(defMaze.end));
-    });
-    await txn.prove();
-    await txn.sign([zkAppPrivateKey, player1Key]).send();
 
     // move
     const moves = [1, 3, 3, 3];
     for (let i = 0; i < moves.length; i++) {
       const move = moves[i];
-      txn = await Mina.transaction(player1, async () => {
+      txn = await Mina.transaction(playerPublicKey, async () => {
         zkApp.play(Field(move));
       });
       await txn.prove();
-      await txn.sign([player1Key]).send();
+      await txn.sign([playerPrivateKey]).send();
 
       if (i < moves.length - 1) {
         const f = zkApp.flag.get();
