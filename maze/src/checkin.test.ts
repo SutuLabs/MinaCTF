eb85a0032330da06101c01473bc1c74a52dae5a4
Liang Shuang <liangshuang@sutu.tech>
2023-05-30 12:10:00 +0000

    init checkin contract

---
+++ maze/src/checkin.test.ts
@@ -0,0 +1,51 @@
+import { CheckinContract } from './checkin';
+import {
+  Field,
+  PrivateKey,
+  PublicKey,
+  Mina,
+  AccountUpdate,
+  Signature,
+} from 'snarkyjs';
+
+describe('checkin', () => {
+  let playerPublicKey: PublicKey,
+    playerPrivateKey: PrivateKey,
+    zkAppAddress: PublicKey,
+    zkAppPrivateKey: PrivateKey;
+
+  beforeEach(async () => {
+    let Local = Mina.LocalBlockchain({ proofsEnabled: false });
+    Mina.setActiveInstance(Local);
+    [{ publicKey: playerPublicKey, privateKey: playerPrivateKey }] =
+      Local.testAccounts;
+    zkAppPrivateKey = PrivateKey.random();
+    zkAppAddress = zkAppPrivateKey.toPublicKey();
+  });
+
+  it('deploys & accepts a correct move', async () => {
+    const zkApp = new CheckinContract(zkAppAddress);
+
+    // deploy
+    let txn = await Mina.transaction(playerPublicKey, () => {
+      AccountUpdate.fundNewAccount(playerPublicKey);
+      zkApp.deploy();
+      zkApp.startGame();
+    });
+    await txn.prove();
+    await txn.sign([zkAppPrivateKey, playerPrivateKey]).send();
+
+    // check contract status
+    const m = zkApp.start.get();
+    expect(m).toEqual(Field(1));
+
+    txn = await Mina.transaction(playerPublicKey, async () => {
+      zkApp.play();
+    });
+    await txn.prove();
+    await txn.sign([playerPrivateKey]).send();
+    const f = zkApp.flag.get();
+    f.assertGreaterThan(0);
+    console.log('flag:', f.toBigInt());
+  });
+});
