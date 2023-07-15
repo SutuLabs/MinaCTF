e684b733501c34a0709cd0498f3207d28b88cc87
LiangShuang <liangshuang@sutu.tech>
2023-07-15 15:05:19 +0800

    localhost also need coi service

--- ui/src/services/registerCoiServcieWorker.ts
+++ ui/src/services/registerCoiServcieWorker.ts
@@ -2,8 +2,8 @@ export {};
 
 function loadCOIServiceWorker() {
   if (
-    typeof window !== 'undefined' &&
-    window.location.hostname != 'localhost'
+    typeof window !== 'undefined'
+    // && window.location.hostname != 'localhost'
   ) {
     const coi = window.document.createElement('script');
     coi.setAttribute('src', '/coi-serviceworker.min.js');
