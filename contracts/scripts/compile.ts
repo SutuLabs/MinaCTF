39d0967c757d031d08f7460271a3a5043bb8a3b4
Liang Shuang <liangshuang@sutu.tech>
2023-07-16 02:48:52 +0000

    compile and generate vkey

--- contracts/scripts/compile.ts
+++ contracts/scripts/compile.ts
@@ -1,5 +1,10 @@
-import { CheckinContract } from '../src/checkin.js';
 import fs from 'fs';
+import { Field } from 'snarkyjs';
+
+import { CheckinContract } from '../src/checkin.js';
+import { MazeContract } from '../src/maze.js';
+import { PrimeContract } from '../src/prime.js';
+import { VerifierContract } from '../src/verifier.js';
 
 const filename = process.argv[2] ?? 'vkey.json';
 const target = process.argv[3] ?? 'checkin';
@@ -7,18 +12,52 @@ const target = process.argv[3] ?? 'checkin';
 const d = fs.readFileSync(filename).toString();
 const vkey = JSON.parse(d);
 
-let verificationKey;
-console.log('Compiling smart contract...');
-console.time('compile');
-if (target == 'checkin') {
-  ({ verificationKey } = await CheckinContract.compile());
-} else {
+if (
+  ['all', 'checkin', 'maze', 'prime', 'verifier'].filter((_) => _ == target)
+    .length == 0
+) {
   throw new Error(`Unknown target: ${target}`);
 }
-console.timeEnd('compile');
-vkey[target] = verificationKey;
+
+let cname;
+
+cname = 'checkin';
+if (target == 'all' || target == cname) {
+  vkey[cname] = await compile(cname, () => CheckinContract.compile());
+}
+
+cname = 'maze';
+if (target == 'all' || target == cname) {
+  vkey[cname] = await compile(cname, () => MazeContract.compile());
+}
+
+cname = 'prime';
+if (target == 'all' || target == cname) {
+  vkey[cname] = await compile(cname, () => PrimeContract.compile());
+}
+
+cname = 'verifier';
+if (target == 'all' || target == cname) {
+  vkey[cname] = await compile(cname, () => VerifierContract.compile());
+}
 
 let data = JSON.stringify(vkey, null, 4);
 fs.writeFileSync(filename, data);
+console.log('Writen to file', filename);
 
-console.log('writen to file ', filename);
+// ================= Functions ======================
+interface VerificationKeyEntity {
+  data: string;
+  hash: Field;
+}
+
+async function compile(
+  name: string,
+  compileHandler: () => Promise<{ verificationKey: VerificationKeyEntity }>
+): Promise<VerificationKeyEntity> {
+  console.log(`Compiling smart contract [${name}]...`);
+  console.time('compile');
+  const { verificationKey } = await compileHandler();
+  console.timeEnd('compile');
+  return verificationKey;
+}
